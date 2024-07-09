98b8bca23ca1c04e3855c265e7b7d4f33584af12
dsampson94 <davesampson15@gmail.com>
2024-07-09 17:01:48 +0200

    BA-03: adds responsive table and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-gray-400">
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Bike } from '@/types/Bike';
 
 const Home = () => {
@@ -16,13 +16,34 @@ const Home = () => {
     }, []);
 
     return (
-        <div>
+        <div className="container min-w-full px-24 pt-8">
             <h1 className="text-2xl font-bold text-center mb-4">Bike List</h1>
-            <ul>
-                {bikes.map(bike => (
-                    <li key={bike.BikeID}>{bike.Make} {bike.Model}</li>
+            <table className="min-w-full text-xs bg-white border border-gray-200">
+                <thead>
+                <tr>
+                    <th className="px-4 py-2 border-b">Make</th>
+                    <th className="px-4 py-2 border-b">Model</th>
+                    <th className="px-4 py-2 border-b">Year</th>
+                    <th className="px-4 py-2 border-b">Displacement</th>
+                    <th className="px-4 py-2 border-b">Price</th>
+                    <th className="px-4 py-2 border-b">Terrain</th>
+                    <th className="px-4 py-2 border-b">Description</th>
+                </tr>
+                </thead>
+                <tbody>
+                {bikes.map((bike) => (
+                    <tr key={bike.BikeID} className="hover:bg-gray-100">
+                        <td className="px-4 py-2 border-b">{bike.Make}</td>
+                        <td className="px-4 py-2 border-b">{bike.Model}</td>
+                        <td className="px-4 py-2 border-b">{bike.Year}</td>
+                        <td className="px-4 py-2 border-b">{bike.Displacement}</td>
+                        <td className="px-4 py-2 border-b">{bike.Price}</td>
+                        <td className="px-4 py-2 border-b">{bike.Terrain}</td>
+                        <td className="px-4 py-2 border-b">{bike.Description}</td>
+                    </tr>
                 ))}
-            </ul>
+                </tbody>
+            </table>
         </div>
     );
 };
